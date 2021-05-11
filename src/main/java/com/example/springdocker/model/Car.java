187875171e8cc645a91a910a63581e4231d53c40
JohanRune <kjgrune@yahoo.se>
2021-05-11 13:39:12 +0200

    Första committen

---
+++ src/main/java/com/example/springdocker/model/Car.java
@@ -0,0 +1,19 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Johan Rune
+ * Date: 2021-05-11
+ * Time: 13:21
+ * Project: IntelliJ IDEA
+ * Copyright: MIT
+ */
+public class Car {
+    String id;
+    String name;
+    boolean delicious;
+    boolean canIDriveIt;
+
+    public String getName() {
+        return name;
+    }
+}
