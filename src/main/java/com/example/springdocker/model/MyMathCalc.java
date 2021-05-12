7b71ecb38a858b4ed52c0b6668c40fdfd09bd852
JohanRune <71837524+JohanRune@users.noreply.github.com>
2021-05-12 15:32:11 +0200

    en commit (#1)

---
+++ src/main/java/com/example/springdocker/model/MyMathCalc.java
@@ -0,0 +1,28 @@
+package com.example.springdocker.model;
+
+/**
+ * Created by Johan Rune
+ * Date: 2021-05-12
+ * Time: 15:20
+ * Project: IntelliJ IDEA
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+    int tala = 9;
+    int talb = 5;
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int multiply (int a, int b){
+        return a*b;
+    }
+
+    public int divide (int a, int b){
+        return a/b;
+    }
+
+
+}
